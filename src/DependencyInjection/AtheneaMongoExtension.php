d3dfd64576a93adc22e9d207343b5ddd113e5fc4
lluc-athenea <56869917+lluc-athenea@users.noreply.github.com>
2024-07-22 09:03:33 +0200

    feat: remove deprecations

--- src/DependencyInjection/AtheneaMongoExtension.php
+++ src/DependencyInjection/AtheneaMongoExtension.php
@@ -2,15 +2,14 @@
 
 namespace Athenea\Mongo\DependencyInjection;
 
-use Athenea\Mongo\Service\MongoService;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class AtheneaMongoExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
         $loader->load('services.yaml');
